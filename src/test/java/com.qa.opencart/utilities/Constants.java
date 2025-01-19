c0011ded675f401f664c1e30d622944360f2966d
mnavyasree1906 <mnavyasree1906@gmail.com>
2025-01-19 22:53:33 +0530

    first commit

--- src/test/java/com.qa.opencart/utilities/Constants.java
+++ src/test/java/com.qa.opencart/utilities/Constants.java
@@ -1,4 +1,8 @@
 package com.qa.opencart.utilities;
 
 public class Constants {
+
+    public static final String USER_DIRECTORY = System.getProperty("user.dir");
+    public static final String CONFIG_DIRECTORY = USER_DIRECTORY + "\\src\\test\\resources\\config.properties";
+    public static final long EXPLICIT_WAIT = 60;
 }
